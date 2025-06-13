9526319d58cf44d259559dd11020c5af5b94aae0
Yeasin Hossain <99527781+yeasinhossain23@users.noreply.github.com>
2025-06-13 20:17:48 +0600

    Update and rename twrp_neon.mk to twrp_KG8.mk

--- twrp_KG8.mk
+++ twrp_KG8.mk
@@ -7,24 +7,24 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from neon device
-$(call inherit-product, device/tecno/neon/device.mk)
+$(call inherit-product, device/tecno/KG8/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := neon
-PRODUCT_NAME := twrp_neon
+PRODUCT_DEVICE := KG8
+PRODUCT_NAME := twrp_KG8
 PRODUCT_BRAND := tecno
-PRODUCT_MODEL := neon
+PRODUCT_MODEL := KG8
 PRODUCT_MANUFACTURER := TECNO
-PRODUCT_RELEASE_NAME := Tecno Spark 8 Pro (neon)
+PRODUCT_RELEASE_NAME := Tecno Spark 8 Pro (KG8)
 
 PRODUCT_GMS_CLIENTID_BASE := android-tecno
 
