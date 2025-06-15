f28b1fcef7b4e162296d3ffa1d6fbfe9671c9b82
Yeasin Hossain <99527781+yeasinhossain23@users.noreply.github.com>
2025-06-15 20:50:37 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,13 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-# 
+# Copyright (C) 2023 The Android Open Source Project
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_KG8.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_KG8.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_KG8-user \
-    twrp_KG8-userdebug \
-    twrp_KG8-eng
+	twrp_KG8-user \
+	twrp_KG8-userdebug \
+	twrp_KG8-eng
+#
